2ab388bcdca17f195c2c09c66a5c7d1422fdacf3
Lucas Pereira <lucas.pereira6c@gmail.com>
2020-10-22 21:30:23 -0300

    Acrescentado documentação e mensagens

--- ReadNFMail.php
+++ ReadNFMail.php
@@ -1,4 +1,13 @@
 <?php
+/**********************************************************
+ * ReadNFMail
+ **********************************************************
+ * Le informações de pagamento de mensagens de e-mail,
+ * e os carrega para API REST.
+ * 
+ * @author: Lucas Pereira (lucas.pereira6c@gmail.com)
+ **********************************************************/
+
 require_once 'classes/creators/ReaderCreator.php';
 require_once 'classes/creators/FileReaderCreator.php';
 require_once 'classes/creators/MailReaderCreator.php';
@@ -17,32 +26,46 @@ use classes\creators\ReaderCreator;
 use classes\creators\MailReaderCreator;
 use classes\creators\FileReaderCreator;
 
-//$path     = "{imap.ethereal.mail:993/imap/tls/novalidade-cert}";
-//$user     = 'arnulfo31@ethereal.email';
-//$password = 'wt77saK6G4CJj1rtSh';
 
 $options  = getopt("p:a:u::w::");
 
-
+// Processamento dos parâmetros de linha de comando
 $apiAddr  = $options['a'];
 $path     = $options['p'];
 $user     = $options['u'];
 $password = $options['w'];
 
+/**
+ * Função para converter o objeto criado em ReaderCreator
+ * 
+ * @param ReaderCreator $creator
+ * @param string $apiAddr
+ */
 function processar (ReaderCreator $creator, string $apiAddr) {
     $api  = new ApiClient($apiAddr);
     foreach ($creator->readAll() as $pay) {
         try {
+            echo "Processanto pagamento " . $pay['id'] . "...\n";
+            echo "================================================\n";
+            echo $pay['payment'];
+            echo "================================================\n";
             $api->sendPayment($pay['payment']);
+            echo "[ OK ]\n\n\n";
             $creator->processed($pay['id']);
         } catch (Exception $ex) {
-            echo "Erro: Pagamento não pôde ser processado. (" . $ex->getMessage() . ")";
+            echo "[ ERRO ]\n";
+            echo "Erro: Pagamento não pôde ser processado. (" . $ex->getMessage() . ")\n\n\n";
         }
     }
 }
 
+// Define qual objeto será utilizado
 if ($user && $password) {
     processar(new MailReaderCreator($path, $user, $password), $apiAddr);
 } else {
     processar(new FileReaderCreator($path), $apiAddr);
 }
+echo "-----------------------------\n";
+echo "FIM DO PROGRAMA\n";
+echo "-----------------------------\n";
+exit (0);
