3f784086ae95fff6df751041c648b7d10bb47fe7
Lucas Pereira <lucas.pereira6c@gmail.com>
2020-10-23 16:34:58 -0300

    Acrescentado testes de mensagem com ordem dos campos alterada.

--- tests/PaymentTest.php
+++ tests/PaymentTest.php
@@ -25,8 +25,31 @@ final class PaymentTest extends TestCase {
         $mail   .= "Vencimento: 12/19\n";
         $mail   .= "Att.\n";
         
-        $nf      = "nota fiscal";      
-        $this->assertInstanceOf(Payment::class, Payment::parsePaymentFromMail($mail, $nf));
+        $nf      = "nota fiscal";
+        $pay1    = Payment::parsePaymentFromMail($mail, $nf);
+        $this->assertInstanceOf(Payment::class, $pay1);
+        $this->assertEquals("Guarida Imóveis", $pay1->getNome());
+        $this->assertEquals("Protásio alves, 1309", $pay1->getEndereco());
+        $this->assertEquals("R$1.300,50", $pay1->getValor());
+        $this->assertEquals("12/19", $pay1->getVencimento());
+        $this->assertEquals("nota fiscal", $pay1->getNF());
+        
+        // Mudando ordem dos campos
+        $mail    = "Bom dia,\n";
+        $mail   .= "Segue meus dados de contato e informações para pagamento\n";
+        $mail   .= "Nome: Guarida Imóveis\n";
+        $mail   .= "Vencimento: 12/19\n";
+        $mail   .= "Valor: R$1.300,50\n";
+        $mail   .= "Endereço: Protásio alves, 1309\n";
+        $mail   .= "Att.\n";
+        
+        $nf      = "nota fiscal";
+        $pay2    = Payment::parsePaymentFromMail($mail, $nf);
+        
+        $this->assertInstanceOf(Payment::class, $pay2);
+        
+        $this->assertEquals($pay1, $pay2);
+        
     }
     
     /**
@@ -34,9 +57,9 @@ final class PaymentTest extends TestCase {
      */
     public function testInvalidPayment() : void {
         $mail = "Mensagem inválida";
-        $arrMail = explode("\n", $mail);
         
         $this->expectError();
-        Payment::parsePaymentFromMail($arrMail, "0");
+        Payment::parsePaymentFromMail($mail, "0");
     }
+    
 }
